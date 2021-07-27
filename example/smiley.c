134abed6ac1446be7b53ffd54cf8b60ee93fe127
sammycage <sammycageagle@gmail.com>
2021-07-27 08:48:47 +0100

    refactoring example

--- example/smiley.c
+++ example/smiley.c
@@ -8,10 +8,11 @@ int main(void)
     plutovg_surface_t* surface = plutovg_surface_create(width, height);
     plutovg_t* pluto = plutovg_create(surface);
 
-    double center_x = width / 2;
-    double center_y = height / 2;
-    double radius = 70;
+    double center_x = width * 0.5;
+    double center_y = height * 0.5;
+    double face_radius = 70;
     double eye_radius = 10;
+    double mouth_radius = 50;
     double eye_offset_x = 25;
     double eye_offset_y = 20;
     double eye_x = center_x - eye_offset_x;
@@ -19,21 +20,26 @@ int main(void)
 
     const double pi = 3.14159265358979323846;
 
-    plutovg_arc(pluto, center_x, center_y, radius, 0, 2 * pi, 0);
+    plutovg_save(pluto);
+    plutovg_arc(pluto, center_x, center_y, face_radius, 0, 2 * pi, 0);
     plutovg_set_source_rgb(pluto, 1, 1, 0);
     plutovg_fill_preserve(pluto);
-    plutovg_set_line_width(pluto, 5);
     plutovg_set_source_rgb(pluto, 0, 0, 0);
+    plutovg_set_line_width(pluto, 5);
     plutovg_stroke(pluto);
+    plutovg_restore(pluto);
 
+    plutovg_save(pluto);
     plutovg_arc(pluto, eye_x, eye_y, eye_radius, 0, 2 * pi, 0);
     plutovg_arc(pluto, center_x + eye_offset_x, eye_y, eye_radius, 0, 2 * pi, 0);
-    plutovg_set_source_rgb(pluto, 0, 0, 0);
     plutovg_fill(pluto);
+    plutovg_restore(pluto);
 
-    plutovg_arc(pluto, center_x, center_y, 50, 0, pi, 0);
-    plutovg_set_source_rgb(pluto, 0, 0, 0);
+    plutovg_save(pluto);
+    plutovg_arc(pluto, center_x, center_y, mouth_radius, 0, pi, 0);
+    plutovg_set_line_width(pluto, 5);
     plutovg_stroke(pluto);
+    plutovg_restore(pluto);
 
     plutovg_surface_write_to_png(surface, "smiley.png");
     plutovg_surface_destroy(surface);
